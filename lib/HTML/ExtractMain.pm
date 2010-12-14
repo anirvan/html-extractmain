27d324ce216bc6167330a435699a7a9fcc7397c9
Anirvan Chatterjee <anirvan@base.mx.org>
2010-12-13 18:32:34 -0800

    Upped version to 0.61

--- lib/HTML/ExtractMain.pm
+++ lib/HTML/ExtractMain.pm
@@ -101,11 +101,11 @@ HTML::ExtractMain - Extract the main content of a web page
 
 =head1 VERSION
 
-Version 0.60
+Version 0.61
 
 =cut
 
-our $VERSION = '0.60';
+our $VERSION = '0.61';
 
 =head1 SYNOPSIS
 
